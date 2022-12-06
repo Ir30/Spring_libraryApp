5610e55a8d150de88e56ccaac99774e5970d5696
irshad007 <irshadmuhammedch8@gmail.com>
2022-12-06 07:04:38 +0530

    view

--- src/main/java/com/example/LibraryApp/controller/library_controller.java
+++ src/main/java/com/example/LibraryApp/controller/library_controller.java
@@ -1,5 +1,6 @@
 package com.example.LibraryApp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,11 @@ public class library_controller {
     @PostMapping("/add")
     public String add (){
         return "add library detiles";
+    }
+
+    @GetMapping("/view")
+    public String view(){
+        return "view book detiles here";
     }
 
 }
